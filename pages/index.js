48258cc8468678fd3053f108604d7c5cb393d615
Mandy Michael <mandymichael@Mandys-MacBook-Pro.local>
2023-11-17 08:13:43 +0800

    Update links on homepage

--- pages/index.js
+++ pages/index.js
@@ -23,20 +23,18 @@ export default function Home({ recentPosts, articles, fontLists, featuredPost })
             <span className={HomeHeader.pageHeadingSubtitle}>for developers</span>
           </h1>
           <h2 className={HomeHeader.intro}>A collection of fun experiments, effects, examples and stuff I have learned as a developer about variable fonts.</h2>
-          <p className={HomeHeader.creator}>Made by <a href="#">Mandy Michael</a> supported by <a href="#">Jello</a>.</p>
+          <p className={HomeHeader.creator}>Made by <a href="https://mandy.dev" target="_blank">Mandy Michael</a> supported by <a href="https://www.instagram.com/adognamedjello" target="_blank">Jello</a>.</p>
           <p className={HomeHeader.volume}><span className={HomeHeader.volumeText}>Vol</span><span className={HomeHeader.volumeNum}>2.0</span></p>
         </section>
 
-      <FeaturedPost featuredPost={featuredPost} />
+        <FeaturedPost featuredPost={featuredPost} />
 
-      <PostList posts={recentPosts} title="Recent Posts" tagType="parent" />
-      <PostList posts={articles} title="Articles" columns={4} postType="article" showMore="true" />
-      <PostList posts={fontLists} title="Font Lists" columns={4} postType="list" showMore="true" />
-
+        <PostList posts={recentPosts} title="Recent Posts" tagType="parent" />
+        <PostList posts={articles} title="Articles" columns={4} postType="article" showMore="true" />
+        <PostList posts={fontLists} title="Font Lists" columns={4} postType="list" showMore="true" />
       </main>
 
       <Footer />
-
     </div>
   );
 }
@@ -57,14 +55,11 @@ export async function getStaticProps() {
     }
   ).slice(0,4);
   
-
   const featuredPost = getSortedPostsData().filter((post) => {
     const lists = post.tags && post.tags.includes('featured');
       return lists;
     }
   ).slice(0,1);
-
-  
 
   return {
     props: {
