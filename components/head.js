c7ba262b88a592f9581a671aa4e622e3b9f3b856
Mandy Michael <mandymichael@Mandys-MacBook-Pro.local>
2023-12-15 13:15:34 +0800

    add facebook and twitter og information

--- components/head.js
+++ components/head.js
@@ -22,6 +22,20 @@ export default function HeadBlock({title, description, url, keywords, image, cus
             <meta property="og:type" content="article" />
             <meta property="og:image" content={image} />
 
+            <meta property="og:url" content={url} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={image} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="twitter:domain" content="variablefonts.dev" />
+            <meta property="twitter:url" content={url} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={image} />
+
+
             <link rel="icon" href="/favicon.ico" />
 
 
